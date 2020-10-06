baf260a8c388c5512160e0a12a74a6c532806fa2
pankaj malik <pankaj_the1@rediffmail.com>
2020-10-06 16:24:17 +0400

    fixing mongodb url

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodbOffline: "mongodb://localhost:27017/test",
   mongodbOnline:
-    "mongodb+srv://@cluster0.f9bia.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv://testuser:test@cluster0.f9bia.mongodb.net/test?retryWrites=true&w=majority",
 };
